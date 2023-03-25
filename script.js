f4b577e79166603fa1dbe67432ca0ac0cdff087b
TheWeirdo <lanedane76@gmail.com>
2023-03-25 14:01:44 -0400

    kinda done

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function test() {
-    document.getElementsByClassName("btn").style.color = "red";
+function simpleColorPicker() {
+    const simpleColors = ["red", "blue", "green", "purple", "cyan", "black", "gray", "orange", "pink"];
+    let rng = Math.floor(Math.random() * simpleColors.length);
+    document.getElementById("color-block").style.backgroundColor = simpleColors[rng];
+    document.getElementById("background-thing").innerText = "Background Color: " + simpleColors[rng].toUpperCase()[0] + simpleColors[rng].substring(1);
 }
