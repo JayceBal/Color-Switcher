8d83bb3cc918f40b1dcb2680a6a78bd02c681319
TheWeirdo <lanedane76@gmail.com>
2023-03-25 20:20:45 -0400

    Yeah its pretty much done

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function simpleColorPicker() {
     document.getElementById("color-block").style.backgroundColor = simpleColors[rng];
     document.getElementById("background-thing").innerText = "Background Color: " + simpleColors[rng].toUpperCase()[0] + simpleColors[rng].substring(1);
 }
+
+function hexColorPicker() {
+    let result = '';
+    const characters = 'abcdef0123456789';
+    let counter = 0;
+    for (let i = 0; i < 6; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    document.getElementById("color-block").style.backgroundColor = "#" + result;
+    document.getElementById("background-thing").innerText = "Background Color: #" + result;
+}
+
+
